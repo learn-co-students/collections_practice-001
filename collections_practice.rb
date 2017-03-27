cdf5a421a123b0e4754b77211ef436117e6ef14a
dremagic <deandre.ellison@gmail.com>
2017-03-27 17:14:01 -0500

    Done.

---
+++ collections_practice.rb
@@ -0,0 +1,71 @@
+def sort_array_asc(array)
+array.sort
+
+end
+
+def sort_array_desc(array)
+  array.sort do |a,b|
+    if a==b
+    0
+    elsif a<b
+    1
+    elsif a>b
+    -1
+    end
+  end
+end
+
+
+def sort_array_char_count(array)
+array.sort do |a,b|
+if a.length == b.length
+  0
+elsif a.length < b.length
+  -1
+elsif a.length > b.length
+  1
+end
+end
+end
+
+
+def swap_elements(array)
+  
+   array[1],array[2] = array[2], array[1]
+   array
+   
+end
+
+def reverse_array(array)
+array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+array.each do |phrase|
+  phrase[2] = "$"
+  new_array << phrase
+
+end
+new_array      
+end
+
+def find_a(array)
+array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+array.reduce(:+)
+  end
+
+  def add_s(array)
+
+    array.each_with_index.collect  do |item, index|
+      if index != 1
+        item <<"s"
+      else
+        item
+      end
+    end
+
+  end
